e9f6edd7e44dd3a1c8c2b9f90a105e730cb13fd0
JohannaMühlenfeld <johanna.muehlenfeld@stud.uni-regensburg.de>
2014-05-28 21:59:46 +0200

    init PlayerManager

---
+++ Thalos/Assets/Scripts/PlayerManager/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController {
+
+
+    private static PlayerController playerController;
+
+    private PlayerModel playerModel;
+
+
+    public static PlayerController Instance()
+    {
+        if (playerController == null)
+        {
+            playerController = new PlayerController();
+        }
+
+        return playerController;
+    }
+
+
+    public PlayerController()
+    {
+        // TODO: fancy init Stuff
+        playerModel = PlayerModel.Instance();
+    }
+
+}
