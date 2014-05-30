eed6dea244c8966d704fa5b2d19bf090276ad5fb
Rameos <martin.dechant@web.de>
2014-05-30 19:20:01 +0200

    Init BackgroundMusicManager

--- Thalos/Assets/Scripts/DebugScripts/Debug_MainMenuButton.cs
+++ Thalos/Assets/Scripts/DebugScripts/Debug_MainMenuButton.cs
@@ -9,18 +9,19 @@ public class Debug_MainMenuButton : MonoBehaviour {
     {
         if (GUI.Button(new Rect(20, 20, 150, 150),"StartGame"))
         {
-            Debug.Log("StartGame");
             GameObject.FindGameObjectWithTag("GameManager").GetComponent<Gamestatemanager>().startGame();
             GameObject.FindGameObjectWithTag("UISoundManager").GetComponent<UISoundManager>().playClickSound();
         }
 
         else if (GUI.Button(new Rect(20, 180, 150, 150), "Options"))
         {
-
+            GameObject.FindGameObjectWithTag("UISoundManager").GetComponent<UISoundManager>().playClickSound();
         }
+
         else if (GUI.Button(new Rect(20, 350, 150, 150), "EndGame"))
         {
-
+            GameObject.FindGameObjectWithTag("UISoundManager").GetComponent<UISoundManager>().playClickSound();
+            GameObject.FindGameObjectWithTag("GameManager").GetComponent<Gamestatemanager>().endGame();
         }
     }
 }
