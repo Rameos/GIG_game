fc1bad63edd8f74f2f51a3e8dd13eb49f1a7ff2b
Rameos <martin.dechant@web.de>
2014-05-30 14:06:01 +0200

    Add DecalSystem

---
+++ Thalos/Assets/Scripts/DebugScripts/Debug_MainMenuButton.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class Debug_MainMenuButton : MonoBehaviour {
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(20, 20, 150, 150),"StartGame"))
+        {
+
+        }
+
+        else if (GUI.Button(new Rect(20, 180, 150, 150), "Options"))
+        {
+
+        }
+        else if (GUI.Button(new Rect(20, 350, 150, 150), "EndGame"))
+        {
+
+        }
+    }
+}
---
+++ Thalos/Assets/Scripts/Effects/FadeSceneEffect.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FadeSceneEffect : MonoBehaviour {
+
+    private float fadeSpeed = 1.5f;
+    private bool sceneStarting = true;
+
+    void Awake()
+    {
+        guiTexture.pixelInset = new Rect(0f, 0f, Screen.width, Screen.height);
+    }
+
+    void Update()
+    {
+        if (sceneStarting)
+            FadeToClear();
+    }
+
+    public void FadeToClear()
+    {
+        guiTexture.color = Color.Lerp(guiTexture.color, Color.clear, fadeSpeed * Time.deltaTime);
+        if (guiTexture.color.a <= 0.05f)
+        {
+            guiTexture.color = Color.clear;
+            guiTexture.enabled = false;
+            sceneStarting = false;
+        }
+    }
+
+    public void FadeToBlack()
+    {
+        guiTexture.enabled = true;
+        guiTexture.color = Color.Lerp(guiTexture.color, Color.black, fadeSpeed * Time.deltaTime);
+    }
+}
