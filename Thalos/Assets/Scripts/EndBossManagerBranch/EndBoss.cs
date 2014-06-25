ca39a51e9812c8ecd024a624b3e188f58dedc0cc
Johanna Mühlenfeld <johanna.muehlenfeld@stud.uni-regensburg.de>
2014-06-25 20:24:56 +0200

    Created Endboss Branch

--- Thalos/Assets/Scripts/EndBossManagerBranch/EndBoss.cs
+++ Thalos/Assets/Scripts/EndBossManagerBranch/EndBoss.cs
@@ -2,11 +2,13 @@
 using System.Collections;
 using Backend;
 
-public class Endboss : BaseEnemy {
+public class Endboss : BaseEnemy
+{
 
-	public Endboss() : base(100, 30, 20)
+    public Endboss()
+        : base(100, 30, 20)
     {
-    
+
     }
 
     public int TakeDamage(int Damage, PlayerModel.DamageTypes DamageType)
