147ba571e2afbbda7bcdb819485fb614438c4a3f
Johanna Mühlenfeld <johanna.muehlenfeld@stud.uni-regensburg.de>
2014-07-02 19:00:22 +0200

    Deleted Armour and Gold in Debug

--- Thalos/Assets/Scripts/DebugScripts/Debug_Playerstats.cs
+++ Thalos/Assets/Scripts/DebugScripts/Debug_Playerstats.cs
@@ -12,10 +12,8 @@ public class Debug_Playerstats : MonoBehaviour {
        GUI.Label(new Rect(20,20,200,20), "HealthPoints: " +  PlayerModel.Instance().HealthPoints);
        GUI.Label(new Rect(20, 40, 200, 20), "Max_HealthPoints: " + PlayerModel.Instance().MaxHealthPoints);
        GUI.Label(new Rect(20, 60, 200, 20), "Phial: " + PlayerModel.Instance().Phial);
-       GUI.Label(new Rect(20, 80, 200, 20), "Gold: " + PlayerModel.Instance().Gold);
        GUI.Label(new Rect(20, 100, 200, 20), "Damage: " + PlayerModel.Instance().Damage);
        GUI.Label(new Rect(20, 120, 200, 20), "DamageType: " + PlayerModel.Instance().DamageType);
-       GUI.Label(new Rect(20, 140, 200, 20), "DamageType: " + PlayerModel.Instance().Armour);
 
 
        if (GUI.Button(new Rect(200, 20, 50, 20), "-10LP"))
