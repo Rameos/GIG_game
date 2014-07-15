b33cceef747fd26c30d5dbcdf8cc6cc4ca077a35
Rameos <martin.dechant@web.de>
2014-07-15 08:43:38 +0200

    Update AI

---
+++ Thalos/Assets/Scripts/Controller/GazeMenuCam.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using iViewX;
+using Controller;
+
+namespace GazeGUI
+{
+    public class GazeMenuCam : MonoBehaviour
+    {
+        Camera guicam;
+
+        void Start()
+        {
+            guicam = gameObject.camera;
+        }
+
+        void Update()
+        {
+
+            Vector2 gazePosOnScreen = (gazeModel.posGazeLeft + gazeModel.posGazeRight) * 0.5f;
+            RaycastHit2D hit = Physics2D.Raycast(guicam.ScreenToWorldPoint(gazePosOnScreen), Vector2.zero);
+
+            if (hit.collider != null)
+            {
+                Debug.Log("Target Position: " + hit.collider.gameObject.name);
+            }
+
+            /*Debug.Log("updateGUICAM");
+
+            
+            
+            
+            RaycastHit2D hit2D = new RaycastHit2D();
+            if (Physics2D.Raycast(new Vector2(123, 123), new Vector2(1, 1)))
+            {
+                if (hit2D != null)
+                {
+                    Debug.Log("Hit2D: " + hit2D.collider.gameObject.name);
+                }
+            }*/
+
+
+
+        }
+    }
+
+}
---
+++ Thalos/Assets/Scripts/Debug_BulletCaster.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using Backend; 
+
+public class Debug_BulletCaster : MonoBehaviour {
+
+    public GameObject bulletInstance;
+    public float updatetimer = 1f;
+
+    [SerializeField]
+    private Transform bulletStartPoint;
+
+    public void shoot(Damage damage)
+    {
+        GameObject bullet = GameObject.Instantiate(bulletInstance,  bulletStartPoint.position, Quaternion.identity) as GameObject;
+        bullet.transform.position = bulletStartPoint.position;
+        bullet.GetComponent<Bullet>().Init(transform.forward, 1, damage);
+    }
+}
