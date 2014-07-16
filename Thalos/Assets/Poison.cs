7421acc8047116f913ba10c3ebac0188e2d10e72
Rameos <martin.dechant@web.de>
2014-07-16 18:05:27 +0200

    Add Basic Effectmanager

---
+++ Thalos/Assets/Poison.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Poison : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter(Collision information)
+    {
+        Debug.Log("Boom");
+        StartCoroutine(explosion());
+    }
+
+    IEnumerator explosion()
+    {
+        yield return new WaitForSeconds(0.1f);
+        
+    }
+
+}
---
+++ Thalos/Assets/Scripts/Effects/EffectManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Effects
+{
+
+    public delegate void startFirePoisonEffect(Vector3 triggerPosition);
+    public delegate void startIcePoisonEffect(Vector3 triggerPosition);
+
+    public class EffectManager : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject effectFirePoison;
+
+        [SerializeField]
+        private GameObject effectIcePoision;
+
+        public static startFirePoisonEffect StartFirePoisonEffectHandler;
+        public static startFirePoisonEffect StartIcePoisonEffectHandler;
+
+
+
+        public static void OnStartFirePoisonEffect(Vector3 triggerPosition)
+        {
+            if (StartFirePoisonEffectHandler != null)
+            {
+                StartFirePoisonEffectHandler(triggerPosition);
+            }
+        }
+
+        public static void OnStartIcePoisonEffect(Vector3 triggerPosition)
+        {
+            if(StartIcePoisonEffectHandler != null)
+            {
+                StartIcePoisonEffectHandler(triggerPosition);
+            }
+
+        }
+
+    }
+}
