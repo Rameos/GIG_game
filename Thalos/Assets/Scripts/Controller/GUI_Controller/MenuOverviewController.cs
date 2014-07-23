3f38ecd8bdee86cb11847d7629e82bb7aefe65f1
Maike Krüger <maikeeefer@googlemail.com>
2014-07-23 04:31:26 +0200

    fixed dependencies

--- Thalos/Assets/Scripts/Controller/GUI_Controller/MenuOverviewController.cs
+++ Thalos/Assets/Scripts/Controller/GUI_Controller/MenuOverviewController.cs
@@ -5,38 +5,42 @@ using Controller;
 
 public class MenuOverviewController : MonoBehaviour {
 
-	//public Texture3D blubb;
-	private int selectionGridInt = 0; 
-	private string[] selectionStrings = {"Inventory", "Mixing"}; 
+	// public Texture3D blubb;
+	// private int toolbarGridInt = 0; 
+	// private string[] toolbarStrings = {"Inventory", "Mixing"}; 
 	
 	void OnGUI() {
 
-		selectionGridInt = GUI.SelectionGrid (new Rect (25, 25, 100, 30), selectionGridInt, selectionStrings, 1); 
+		// toolbarGridInt = GUI.Toolbar (new Rect (25, 30, 100, 40), toolbarGridInt, toolbarStrings); 
 
-		if (GUI.changed) 
-		{
-			if(0 == selectionGridInt) 
-			{
-				print("Inventory selected");
-			} 
-			else 
-			{
-				print ("Mixing selected"); 
-			}
+		// if (GUI.changed) 
+		// {
+		// if(0 == toolbarGridInt) 
+		// {
+		// print("Inventory selected");
+				//Application.LoadLevel(5); 
+		// GameObject.FindGameObjectWithTag("UISoundManager").GetComponent<UISoundManager>().playClickSound();
+		// } 
+		// else 
+		// {
+		// print ("Mixing selected"); 
+				//Application.LoadLevel(4); 
+		// GameObject.FindGameObjectWithTag("UISoundManager").GetComponent<UISoundManager>().playClickSound();
+		// }
+		// }
+
+		// GUI.BeginGroup (new Rect(Screen.width / 2 - 250, Screen.height / 2 - 250, 500, 500)); 		
+		GUI.Box (new Rect (Screen.width / 2 - 250, Screen.height / 2 - 250, 650, 500), "Overview");		
+		if (GUI.Button (new Rect (Screen.width / 2 - 100, Screen.height / 2, 150, 150), "Inventory")) {
+			// Application.LoadLevel(5); 
+			print ("Load menu for inventory");
+			GameObject.FindGameObjectWithTag("UISoundManager").GetComponent<UISoundManager>().playClickSound();
 		}
-
-		// GUI.BeginGroup (new Rect(Screen.width / 2 - 50, Screen.height / 2 - 50, 300, 300)); 		
-		// GUI.Box (new Rect (0, 0, 300, 300), "Overview");		
-		// if (GUI.Button (new Rect (20, 40, 80, 20), "Inventory")) {
-			// Application.LoadLevel(1); 
-			// Inventory laden
-		//	print ("Load menu for inventory");
-		// }
-		// if (GUI.Button(new Rect(20,70,80,20), "Mixing")) {
-			// Application.LoadLevel(2);
-			// RingMenuController laden
-		//	print ("Load Menu for mixing (RingMenuController)");
-		// }
+		else if (GUI.Button(new Rect(Screen.width / 2 + 100,Screen.height / 2,150,150), "Mixing")) {
+			// Application.LoadLevel(4);
+			print ("Load Menu for mixing");
+			GameObject.FindGameObjectWithTag("UISoundManager").GetComponent<UISoundManager>().playClickSound();
+		}
 		// GUI.EndGroup (); 
 	}
 }
--- Thalos/Assets/Scripts/Controller/GUI_Controller/WeaponsMenuController.cs
+++ Thalos/Assets/Scripts/Controller/GUI_Controller/WeaponsMenuController.cs
@@ -7,19 +7,5 @@ public class WeaponsMenuController : MonoBehaviour {
 	
 	void OnGUI() {
 
-		//Make a background box
-		GUI.Box (new Rect (10, 10, 100, 90), "Loader Menu"); 
-
-		//Make the first button. If it is pressed, Application.LoadLevel will be executed
-		if (GUI.Button (new Rect (20, 40, 80, 20), "Level 1")) {
-			Application.LoadLevel(1); 
-			GameObject.FindGameObjectWithTag("UISoundManager").GetComponent<UISoundManager>().playClickSound();
-			}
-
-		// Make the second button.
-		if(GUI.Button(new Rect(20,70,80,20), "Level 2")) {
-			Application.LoadLevel(2);
-			GameObject.FindGameObjectWithTag("UISoundManager").GetComponent<UISoundManager>().playClickSound();
-		}
 	}
 }
