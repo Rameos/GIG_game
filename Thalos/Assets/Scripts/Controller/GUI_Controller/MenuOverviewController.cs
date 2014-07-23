00b830b5e66c3419e3bcbd936dc6c334b3c32466
Maike Krüger <maikeeefer@googlemail.com>
2014-07-23 02:57:43 +0200

    added controller for different menu types

---
+++ Thalos/Assets/Scripts/Controller/GUI_Controller/MenuOverviewController.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using System.Collections;
+using iViewX;
+using Controller;
+
+public class MenuOverview : MonoBehaviour {
+
+	//public Texture3D blubb;
+	private int selectionGridInt = 0; 
+	private string[] selectionStrings = {"Inventory", "Mixing"}; 
+	
+	void OnGUI() {
+
+		selectionGridInt = GUI.SelectionGrid (new Rect (25, 25, 100, 30), selectionGridInt, selectionStrings, 1); 
+
+		if (GUI.changed) 
+		{
+			if(0 == selectionGridInt) 
+			{
+				print("Inventory selected");
+			} 
+			else 
+			{
+				print ("Mixing selected"); 
+			}
+		}
+
+		// GUI.BeginGroup (new Rect(Screen.width / 2 - 50, Screen.height / 2 - 50, 300, 300)); 		
+		// GUI.Box (new Rect (0, 0, 300, 300), "Overview");		
+		// if (GUI.Button (new Rect (20, 40, 80, 20), "Inventory")) {
+			// Application.LoadLevel(1); 
+			// Inventory laden
+		//	print ("Load menu for inventory");
+		// }
+		// if (GUI.Button(new Rect(20,70,80,20), "Mixing")) {
+			// Application.LoadLevel(2);
+			// RingMenuController laden
+		//	print ("Load Menu for mixing (RingMenuController)");
+		// }
+		// GUI.EndGroup (); 
+	}
+}
---
+++ Thalos/Assets/Scripts/Controller/GUI_Controller/MixingMenuController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using iViewX;
+using Controller;
+
+public class MainMenuController : MonoBehaviour {
+
+	void OnGUI() {
+
+		//Make a background box
+		GUI.BeginGroup (new Rect(Screen.width / 2 - 50, Screen.height / 2 - 50,  Screen.width, Screen.height)); 
+
+		GUI.Box (new Rect (0, 0, Screen.width / 2, Screen.height / 2), "Loader Menu");
+
+		if (GUI.Button (new Rect (20, 40, 80, 20), "Main Menu")) {
+			Application.LoadLevel(1); 
+			}
+
+		// Make the second button.
+		if(GUI.Button(new Rect(20,70,80,20), "Starte Demoversion")) {
+			Application.LoadLevel(2);
+		}
+
+		if(GUI.Button(new Rect(20,90,80,20), "Inventory")) {
+			//Application.LoadLevel(2);
+			print("Inventory clicked"); 
+		}
+
+		if(GUI.Button(new Rect(20,120,80,20), "Mixing")) {
+			//Application.LoadLevel(2);
+			print ("Mixing clicked");
+		}
+		
+		GUI.EndGroup (); 
+	}
+}
