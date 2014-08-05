ab29152b237b1fffd3b6c3726f6795ffea228ec1
Rameos <martin.dechant@web.de>
2014-08-05 15:38:56 +0200

    Add ExplosionForce

---
+++ Thalos/Assets/createExplosion.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class createExplosion : MonoBehaviour {
+
+    [SerializeField]
+    private float force;
+    
+    [SerializeField]
+    private float radius; 
+
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(delayExplosion());
+	}
+	
+    IEnumerator delayExplosion()
+    {
+        yield return new WaitForSeconds(0.5f);
+                Collider[] colliders = Physics.OverlapSphere(transform.position, radius);
+
+        foreach(Collider colliderItem in colliders)
+        {
+            if (colliderItem.rigidbody == null)
+            {
+                continue; 
+            }
+
+            colliderItem.rigidbody.AddExplosionForce(force, transform.position, radius, 1, ForceMode.Impulse);
+        }
+    }
+}
