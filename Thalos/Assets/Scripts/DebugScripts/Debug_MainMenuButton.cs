5493d76b15e974932c338024913a39ffdc1b00ed
Rameos <martin.dechant@web.de>
2014-08-11 16:48:05 +0200

    Add Sprites to  Main menu

--- Thalos/Assets/Scripts/DebugScripts/Debug_MainMenuButton.cs
+++ Thalos/Assets/Scripts/DebugScripts/Debug_MainMenuButton.cs
@@ -6,20 +6,25 @@ using Controller;
 [ExecuteInEditMode]
 public class Debug_MainMenuButton : MonoBehaviour {
 
+    [SerializeField]
+    private float posX = 600;
+
+    [SerializeField]
+    private float posY = 150;
     void OnGUI()
     {
-        if (GUI.Button(new Rect(20, 20, 150, 150),"StartGame"))
+        if (GUI.Button(new Rect(posX, posY, 150, 150), "StartGame"))
         {
             GameObject.FindGameObjectWithTag("GameManager").GetComponent<Gamestatemanager>().startGame();
             GameObject.FindGameObjectWithTag("UISoundManager").GetComponent<UISoundManager>().playClickSound();
         }
 
-        else if (GUI.Button(new Rect(20, 180, 150, 150), "Options"))
+        else if (GUI.Button(new Rect(posX, posY+150, 150, 150), "Options"))
         {
             GameObject.FindGameObjectWithTag("UISoundManager").GetComponent<UISoundManager>().playClickSound();
         }
 
-        else if (GUI.Button(new Rect(20, 350, 150, 150), "EndGame"))
+        else if (GUI.Button(new Rect(posX, posY+300, 150, 150), "EndGame"))
         {
             GameObject.FindGameObjectWithTag("UISoundManager").GetComponent<UISoundManager>().playClickSound();
             GameObject.FindGameObjectWithTag("GameManager").GetComponent<Gamestatemanager>().endGame();
