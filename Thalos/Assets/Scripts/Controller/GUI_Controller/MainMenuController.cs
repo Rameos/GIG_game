e8e9b72c17b74a5094919b1a5cd1b8d53d2d9763
Rameos <martin.dechant@web.de>
2014-08-13 14:52:02 +0200

    Add MainMenu

--- Thalos/Assets/Scripts/Controller/GUI_Controller/MainMenuController.cs
+++ Thalos/Assets/Scripts/Controller/GUI_Controller/MainMenuController.cs
@@ -5,51 +5,30 @@ using Controller;
 
 public class MainMenuController : MonoBehaviour {
 
-	void OnGUI() {
+    private GameObject[] buttonsMainmenu;
+    public Color32 colorText { get; set; }
+    public Color32 colorSprites { get; set; }
 
-		GUI.BeginGroup (new Rect(Screen.width / 2 - 50, Screen.height / 2 - 50,  Screen.width, Screen.height)); 
+    [SerializeField]
+    private float delay;
+    private bool isFadeActive = false;
 
-		GUI.Box (new Rect (0, 0, Screen.width / 2, Screen.height / 2), "Loader Menu");
+    void Start()
+    {
+        buttonsMainmenu = GameObject.FindGameObjectsWithTag("MainMenu");
 
-		if (GUI.Button (new Rect (20, 50, 100, 30), "Main Menu")) 
-		{
-			Application.LoadLevel(1); 
-			GameObject.FindGameObjectWithTag("UISoundManager").GetComponent<UISoundManager>().playClickSound();
-		}
 
-		else if(GUI.Button(new Rect(20,100,100,30), "Start Game"))
-		{
-			Application.LoadLevel(2);
-			GameObject.FindGameObjectWithTag("UISoundManager").GetComponent<UISoundManager>().playClickSound();
-		}
+    }
 
-		else if(GUI.Button(new Rect(20,150,100,30), "MenuOverview")) 
-		{
-			//Application.LoadLevel(3);
-			print ("Mixing clicked");
-			GameObject.FindGameObjectWithTag("UISoundManager").GetComponent<UISoundManager>().playClickSound();
-		}
 
-		else if(GUI.Button(new Rect(20,200,100,30), "Mixing")) 
-		{
-			//Application.LoadLevel(4);
-			print ("Mixing clicked");
-			GameObject.FindGameObjectWithTag("UISoundManager").GetComponent<UISoundManager>().playClickSound();
-		}
 
-		else if(GUI.Button(new Rect(20,250,100,30), "Inventory")) 
-		{
-			//Application.LoadLevel(5);
-			print("Inventory clicked"); 
-			GameObject.FindGameObjectWithTag("UISoundManager").GetComponent<UISoundManager>().playClickSound();
-		}
+    private void OnMouseOverItem()
+    {
 
-		else if (GUI.Button(new Rect(20, 350, 150, 150), "End Game"))
-		{
-			GameObject.FindGameObjectWithTag("UISoundManager").GetComponent<UISoundManager>().playClickSound();
-			GameObject.FindGameObjectWithTag("GameManager").GetComponent<Gamestatemanager>().endGame();
-		}
-		
-		GUI.EndGroup (); 
-	}
+    }
+
+    private void OnMouseDown()
+    {
+
+    }
 }
